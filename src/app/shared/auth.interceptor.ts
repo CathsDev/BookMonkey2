eb1dc796ea5afb816f035f648ace773ac3710b79
c.sauer <caths.dev@gmail.com>
2024-10-23 09:46:27 +0200

    add authentificator, login, logout

---
+++ src/app/shared/auth.interceptor.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpHandler, HttpEvent, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+    constructor(private authService: AuthService) {}
+
+    intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+        const token = '1234567890';
+
+        if (this.authService.isAuthenticated) {
+            // Token im Header einfügen
+            const reqWithToken = request.clone({
+                setHeaders: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            return next.handle(reqWithToken);
+        } else {
+            // Request unverändert weitergeben
+            return next.handle(request);
+        }
+
+    }
+}
