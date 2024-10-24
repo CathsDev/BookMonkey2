38c082b40b6275a592b86dacb56e15cd44780ca6
c.sauer <caths.dev@gmail.com>
2024-10-24 11:10:56 +0200

    add reactive forms and book-edit

--- src/app/admin/book-form/book-form.component.ts
+++ src/app/admin/book-form/book-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { Book } from '../../shared/book';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector: 'bm-book-form',
@@ -7,16 +8,63 @@ import { Book } from '../../shared/book';
     styleUrl: './book-form.component.css'
 })
 export class BookFormComponent {
-    book: Book = {
-        isbn: '',
-        title: '',
-        authors: ['']
-    };
 
     @Output() submitBook = new EventEmitter<Book>();
 
+    form = new FormGroup({
+        title: new FormControl('', {
+            nonNullable: true,
+            validators: Validators.required
+        }),
+        subtitle: new FormControl('', {
+            nonNullable: true
+        }),
+        isbn: new FormControl('', {
+            nonNullable: true,
+            validators: [
+                Validators.required,
+                Validators.minLength(10),
+                Validators.maxLength(13),
+            ]
+        }),
+        description: new FormControl('', {
+            nonNullable: true
+        }),
+        published: new FormControl('', {
+            nonNullable: true
+        }),
+        thumbnailUrl: new FormControl('', {
+            nonNullable: true
+        }),
+        authors: this.buildAuthorsArray(['']),
+    });
+
+    get authors() {
+        return this.form.controls.authors;
+    }
+
+    private buildAuthorsArray(authors: string[]) {
+        return new FormArray(
+            authors.map(v => new FormControl(v, {nonNullable: true}))
+        );
+    }
+
+    addAuthorControl() {
+        this.authors.push(
+            new FormControl('', {
+                nonNullable: true
+            })
+        );
+    }
+
     submitForm() {
-        this.submitBook.emit(this.book);
+        const formValue = this.form.getRawValue();
+        const authors = formValue.authors.filter(author => !!author);
+        const newBook: Book = {
+            ...formValue,
+            authors
+        };
+        this.submitBook.emit(newBook);
     }
 
 }
