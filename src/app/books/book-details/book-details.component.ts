1ec9213bac0586bf69d2d7d3ea4fd809af81a14b
c.sauer <caths.dev@gmail.com>
2024-10-28 14:13:55 +0100

    new structure for standalonde

--- src/app/books/book-details/book-details.component.ts
+++ src/app/books/book-details/book-details.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
+import { AsyncPipe,DatePipe, NgFor, NgIf } from '@angular/common';
 import { Book } from '../../shared/book';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { BookStoreService } from '../../shared/book-store.service';
 import { Observable } from 'rxjs';
+import { IsbnPipe } from '../../shared/isbn.pipe';
+import { LoggedinOnlyDirective } from '../../shared/loggedin-only.directive';
+import { ConfirmDirective } from '../../shared/confirm.directive';
 
 @Component({
     selector: 'bm-book-details',
     templateUrl: './book-details.component.html',
-    styleUrl: './book-details.component.css'
+    styleUrl: './book-details.component.css',
+    standalone: true,
+    imports: [
+        NgIf, NgFor, DatePipe, AsyncPipe, RouterLink, IsbnPipe, LoggedinOnlyDirective, ConfirmDirective
+    ]
 })
 export class BookDetailsComponent {
     book$: Observable<Book>;
--- src/app/books/book-list/book-list.component.ts
+++ src/app/books/book-list/book-list.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { Book } from '../../shared/book';
 import { BookStoreService } from '../../shared/book-store.service';
 import { Observable } from 'rxjs';
+import { AsyncPipe, NgFor, NgIf } from '@angular/common';
+import { BookListItemComponent } from '../book-list-item/book-list-item.component';
 
 @Component({
   selector: 'bm-book-list',
   templateUrl: './book-list.component.html',
-  styleUrl: './book-list.component.css'
+  styleUrl: './book-list.component.css',
+    standalone: true,
+    imports: [
+        AsyncPipe, NgFor, NgIf, BookListItemComponent
+    ]
 })
 export class BookListComponent {
     books$ : Observable<Book[]>;
